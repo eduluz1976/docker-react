6cd293eeadcfe71fb4f018265bca5086d87da802
Eduardo Luz <eduluz1976@gmail.com>
2018-12-10 23:01:32 -0800

    Small adjustments of files...

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            
+          ...
+
+
           </a>
         </header>
       </div>
